f9f4401d97d5d32f58e4cc0e782f68981866fda4
SHIVAM YADAV <shivamyadavskn@gmail.com>
2022-11-23 18:12:18 +0530

    quizapps

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(App);
+}
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
