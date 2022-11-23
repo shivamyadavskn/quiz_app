a4f3ceae464066d41bac33d0bf0942b16d663a7d
SHIVAM YADAV <shivamyadavskn@gmail.com>
2022-11-23 19:07:51 +0530

    quizupdates1

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,51 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
- class MyApp extends StatelessWidget {
-   const MyApp({Key? key}) : super(key: key);
- 
-   @override
-   Widget build(BuildContext context) {
-     return Container();
-   }
- }
- 
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Quiz App',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    List<String> question = [
+      "Which is your color",
+      "Which is your best number"
+    ];
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Text(question[0]),
+          TextButton(
+            onPressed: () {},
+            child: const Text("red"),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: const Text("yellow"),
+          ),
+          TextButton(
+            onPressed: () {
+              print("second option");
+            },
+            child: const Text("green"),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: const Text("white"),
+          ),
+        ],
+      ),
+    );
+  }
+}
